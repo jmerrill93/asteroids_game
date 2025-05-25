d7e4548e95f8c124364578ab338e588080feefb9
jmerrill93 <merrill.joshua1@gmail.com>
2025-05-25 05:44:59 -0400

    added external window to open up and display a black screen

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import pygame
 from constants import *
+import sys
 
 def main():
+    pygame.init()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    #screen setup
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids: A Project by BootDev")
+    #main loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
