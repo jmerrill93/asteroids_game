d07274bfd4da84eba036b7d3819ffc75e8f11dc4
jmerrill93 <merrill.joshua1@gmail.com>
2025-05-25 06:52:17 -0400

    added game time clock to control refresh rate.

--- main.py
+++ main.py
@@ -10,14 +10,19 @@ def main():
     #screen setup
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Asteroids: A Project by BootDev")
+    
+    #time clock
+    clock = pygame.time.Clock()
+    dt = 0
+    
     #main loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        screen.fill((0, 0, 0))
+        screen.fill("black")
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000.0
 if __name__ == "__main__":
     main()
